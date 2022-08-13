077107f2f7c3e107b80723a792845aaefde70b28
Matt Arnold <matt.arnd@gmail.com>
2022-08-13 08:59:07 -0500

    added methods to bst

--- data_structures/BinarySearchTree/BinarySearchTree.js
+++ data_structures/BinarySearchTree/BinarySearchTree.js
@@ -47,6 +47,80 @@ class BinarySearchTree {
 
 
 
+  /**
+    * BFS order: horizontal rows top-down left-to-right.
+    * Converts this BST into an array following Breadth First Search order.
+    * Example on the fullTree var:
+    * [25, 15, 50, 10, 22, 35, 70, 4, 12, 18, 24, 31, 44, 66, 90]
+    * @param {Node} current The current node during the traversal of this tree.
+    * @returns {Array<number>} The data of all nodes in BFS order.
+    */
+   toArrLevelorder(current = this.root) {
+
+    const queue = []
+    const vals = []
+
+    if (current) {
+      queue.push(current)
+    }
+
+    while (queue.length > 0) {
+      const dequeueNode = queue.shift();
+      vals.push(dequeueNode.data);
+      if (dequeueNode.left) {
+        queue.push(dequeueNode.left)
+      }
+      if (dequeueNode.right) {
+        queue.push(dequeueNode.right)
+      }
+
+    }
+
+    return vals;
+   }
+
+   /**
+    * Recursively counts the total number of nodes in this tree.
+    * - Time: O(?).
+    * - Space: O(?).
+    * @param {Node} node The current node during the traversal of this tree.
+    * @returns {number} The total number of nodes.
+    */
+   size(node = this.root) {}
+
+   /**
+    * Calculates the height of the tree which is based on how many nodes from
+    * top to bottom (whichever side is taller).
+    * - Time: O(?).
+    * - Space: O(?).
+    * @param {Node} node The current node during traversal of this tree.
+    * @returns {number} The height of the tree.
+    */
+   height(node = this.root) {}
+
+   /**
+    * Determines if this tree is a full tree. A full tree is a tree where every
+    * node has both a left and a right except for the leaf nodes (last nodes)
+    * - Time: O(?).
+    * - Space: O(?).
+    * @param {Node} node The current node during traversal of this tree.
+    * @returns {boolean} Indicates if this tree is full.
+    */
+   isFull(node = this.root) {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
    /**
      * DFS Preorder: (CurrNode, Left, Right)
      * Converts this BST into an array following Depth First Search preorder.
